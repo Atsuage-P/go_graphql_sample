6e2a7e71d9da259b33aa643500585f4c11c7bbce
atsuage <atsuage.programmer@gmail.com>
2024-09-03 19:42:40 +0900

    user関連の追記

--- graph/services/service.go
+++ graph/services/service.go
@@ -11,16 +11,26 @@ type Services interface {
 	UserService
 }
 
-type services struct {
-	*userService
+type UserService interface {
+	GetUserByID(ctx context.Context, id string) (*model.User, error)
+	GetUserByName(ctx context.Context, name string) (*model.User, error)
+	ListUsersByID(ctx context.Context, IDs []string) ([]*model.User, error)
 }
 
-type UserService interface {
-	GetUserByName(ctx context.Context, name string) (*model.User, error)
+// type IssueService interface {
+// 	GetIssueByID(ctx context.Context, id string) (*model.Issue, error)
+// 	GetIssueByRepoAndNumber(ctx context.Context, repoID string, number int) (*model.Issue, error)
+// 	ListIssueInRepository(ctx context.Context, repoID string, after *string, before *string, first *int, last *int) (*model.IssueConnection, error)
+// }
+
+type services struct {
+	*userService
+	// *issueService
 }
 
 func New(exec boil.ContextExecutor) Services {
 	return &services{
 		userService: &userService{exec: exec},
+		// issueService: &issueService{exec: exec},
 	}
 }
